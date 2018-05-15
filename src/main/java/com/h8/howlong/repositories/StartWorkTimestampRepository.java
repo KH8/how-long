66d90cb2a706f8d9755d375f8d4999b09f6341a6
Krzysztof Reczek <krzysztof.reczek.ctr@sabre.com>
2018-05-15 15:46:03 +0200

    File initialization fixed

--- src/main/java/com/h8/howlong/repositories/StartWorkTimestampRepository.java
+++ src/main/java/com/h8/howlong/repositories/StartWorkTimestampRepository.java
@@ -3,21 +3,25 @@ package com.h8.howlong.repositories;
 import com.h8.howlong.domain.StartWorkTimestamp;
 
 import java.io.*;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public class StartWorkTimestampRepository {
 
-    private final static String DB_FILE_NAME = "./_db";
+    private final static String DB_FILE_NAME = System.getProperty("user.home") + "/_db";
 
     public StartWorkTimestampRepository() {
         try {
             File yourFile = new File(DB_FILE_NAME);
             if (yourFile.createNewFile()) {
-                System.out.println("Created file: " + DB_FILE_NAME);
+                save(StartWorkTimestamp.builder()
+                        .timestamp(LocalDateTime.MIN)
+                        .build());
+                System.out.println("Initialized file: " + DB_FILE_NAME);
             }
         } catch (IOException e) {
-            e.printStackTrace();
             System.out.println("ERROR: Could not create file: " + DB_FILE_NAME);
+            e.printStackTrace();
         }
     }
 
