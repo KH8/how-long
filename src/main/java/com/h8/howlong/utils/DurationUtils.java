f30dbf89d2fd70af27dd95e77d333c34fb002f45
Krzysztof Reczek <krzysztof.reczek.ctr@sabre.com>
2018-05-22 16:29:25 +0200

    Fixed printing of negative durations

--- src/main/java/com/h8/howlong/utils/DurationUtils.java
+++ src/main/java/com/h8/howlong/utils/DurationUtils.java
@@ -5,9 +5,10 @@ import java.time.Duration;
 public class DurationUtils {
 
     public static String format(Duration d) {
-        long hours = d.toHours();
-        long minutes = d.minusHours(hours).toMinutes();
-        return String.format("%02d:%02d", hours, minutes);
+        long hours = d.abs().toHours();
+        long minutes = d.abs().minusHours(hours).toMinutes();
+        return String.format("%s%02d:%02d",
+                d.isNegative() ? "-" : "", hours, minutes);
     }
 
 }
