afd75bcac3a376c6bd9a537b24009db1b97e5a8e
Krzysztof Reczek <krzysztof.reczek.ctr@sabre.com>
2018-07-25 13:13:01 +0200

    F01 - Fix run script

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-java -jar how-long-app/target/how-long-app-0.0.1-SNAPSHOT.jar $1 $2 2> /dev/null
+BASEDIR=$(dirname "$0")
+java -jar $BASEDIR/how-long-app/target/how-long-app-0.0.1-SNAPSHOT.jar $1 $2 2> /dev/null
