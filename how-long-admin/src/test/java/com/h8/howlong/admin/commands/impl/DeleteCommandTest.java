56a0f65885f52c393fec5eb1f7bb6575197f52a1
Michal <1milosz2@gmail.com>
2018-10-13 17:48:42 +0200

    DeleteCommandTest class implemented

--- how-long-admin/src/test/java/com/h8/howlong/admin/commands/impl/DeleteCommandTest.java
+++ how-long-admin/src/test/java/com/h8/howlong/admin/commands/impl/DeleteCommandTest.java
@@ -1,7 +1,67 @@
 package com.h8.howlong.admin.commands.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.h8.howlong.admin.commands.CommandResultStatus;
+import com.h8.howlong.admin.services.TimesheetManagementFailedException;
+import com.h8.howlong.admin.services.TimesheetManagementService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.*;
 
 class DeleteCommandTest {
+
+    private TimesheetManagementService timesheetManagementService;
+
+    private DeleteCommand deleteCommand;
+
+    private int month;
+    private int day;
+
+    @BeforeEach
+    void setUp() {
+        timesheetManagementService = mock(TimesheetManagementService.class);
+        month = 9;
+        day = 30;
+        deleteCommand = new DeleteCommand(timesheetManagementService, month, day);
+    }
+
+    @Test
+    void shouldCallDeleteOnTimesheetManagementServiceAndReturnCommandResultOk()
+            throws TimesheetManagementFailedException {
+        //when
+        var commandResult = deleteCommand.execute();
+
+        //then
+        var monthCaptor = ArgumentCaptor.forClass(Integer.class);
+        var dayCaptor = ArgumentCaptor.forClass(Integer.class);
+        verify(timesheetManagementService, times(1)).delete(monthCaptor.capture(), dayCaptor.capture());
+
+        assertThat(monthCaptor.getValue()).isEqualTo(month);
+        assertThat(dayCaptor.getValue()).isEqualTo(day);
+        assertThat(commandResult)
+                .hasFieldOrPropertyWithValue("message", "The day '30'.'9' has been deleted");
+        assertThat(commandResult)
+                .hasFieldOrPropertyWithValue("status", CommandResultStatus.SUCCESS);
+    }
+
+    @Test
+    void shouldReturnCommandResultErrorWhenTimeSheetManagementExceptionIsThrown()
+            throws TimesheetManagementFailedException {
+        //when
+        doThrow(new TimesheetManagementFailedException("test"))
+                .when(timesheetManagementService).delete(month, day);
+
+        var commandResult = deleteCommand.execute();
+
+        //then
+        assertThat(commandResult)
+                .hasFieldOrPropertyWithValue("message", "The day '30'.'9' could not be deleted because of an exception: test");
+        assertThat(commandResult)
+                .hasFieldOrPropertyWithValue("status", CommandResultStatus.ERROR);
+    }
+
+
 
 }
