d403217431436a3dccfa04f731a729f689966d11
1milosz2 <1milosz2@gmail.com>
2018-11-03 21:08:15 +0100

    F03 Logging to log file.

--- how-long-utils/src/main/java/com/h8/howlong/utils/Logger.java
+++ how-long-utils/src/main/java/com/h8/howlong/utils/Logger.java
@@ -1,28 +1,35 @@
 package com.h8.howlong.utils;
 
 
-import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Logger {
 
-    private static final String LOG_FILE_NAME = "_HowLongLog.log";
-
-    private static final String LOG_FILE_PATH = System.getProperty("user.home");
-
+    private static final Path LOG_FILE = Paths.get(System.getProperty("user.home"), "_how_long.log");
 
     public static void log(String message, Object... args) {
-        var m = setUpLogMessage(message, args);
+        var m = resolveLogMessage(message, args);
+        logToConsole(m);
+        m = applyDatePrefix(m);
         logToFile(m);
     }
 
+    private static String resolveLogMessage(String message, Object... args) {
+        var m = message;
+        for (Object a : args) {
+            m = m.replaceFirst("\\{}", a.toString());
+        }
+        return m;
+    }
+
     private static void logToFile(String message) {
-        var logFile = new File(Paths.get(LOG_FILE_PATH, LOG_FILE_NAME).toString());
-        try (var pw = new PrintWriter(logFile)) {
+        try (var pw = new PrintWriter(new FileWriter(LOG_FILE.toString(), true))) {
             pw.println(message);
         } catch (IOException e) {
             System.out.println("ERROR: Could not log to a file.");
@@ -30,21 +37,16 @@ public class Logger {
         }
     }
 
-    private static String setUpLogMessage(String message, Object... args) {
-        var m = message;
-        for (Object a : args) {
-            m = m.replaceFirst("\\{}", a.toString());
-        }
-        return new StringBuilder()
-                .append(formatCurrentLocalDateTime())
-                .append(System.lineSeparator())
-                .append(m)
-                .toString();
+    private static void logToConsole(String message) {
+        System.out.println(message);
     }
 
-    private static String formatCurrentLocalDateTime() {
-        var now = LocalDateTime.now();
+    private static String applyDatePrefix(String m) {
+        return String.format("%s %s", getCurrentDate(), m);
+    }
+
+    private static String getCurrentDate() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss dd-MM-yyyy");
-        return now.format(formatter);
+        return LocalDateTime.now().format(formatter);
     }
 }
